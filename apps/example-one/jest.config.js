d5dc259690985f468a7f00400915d449a58dae10
psl <polischuk.post@gmail.com>
2020-09-29 15:06:20 +0300

    initial commit

---
+++ apps/example-one/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'example-one',
+  preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  coverageDirectory: '../../coverage/apps/example-one',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
